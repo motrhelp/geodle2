f07e7c6eb61f98f9c4d779b4d0112b5973f2d441
Stan K <motrhelp@stans-air.home>
2022-06-08 21:45:40 +0200

    Refactor first level

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import GuessFlag from './components/GuessFlag'
-
-const blackAndWhiteTheme = createTheme({
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#212121',
-    },
-    secondary: {
-      main: '#dd2c00',
-    },
-  },
-});
-
+import GuessFlagScreen from './components/GuessFlagScreen'
+import { countriesWithFlags } from './data/CountryList';
+import { gameNumber } from './util/GameNumber'
 
 function App() {
+  const blackAndWhiteTheme = createTheme({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#ffffff',
+      },
+      secondary: {
+        main: '#dd2c00',
+      },
+    },
+  });
+
+  // The country to guess
+  const country = countriesWithFlags[gameNumber];
+
   return (
     <ThemeProvider theme={blackAndWhiteTheme} >
       <CssBaseline enableColorScheme />
-      <GuessFlag />
+      <GuessFlagScreen country={country} />
     </ThemeProvider>
   );
 }
